4abfb58cd7bfcd7a4e1ad92603124dbe14a3f7b0
Catherine1402 <144625881+Catherine1402@users.noreply.github.com>
2023-10-22 22:16:36 +0000

    Generator finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net']
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+generateAllCombinations(pronoun, adj, noun, extensions);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateAllCombinations(arr1, arr2, arr3, arr4) {
+  const res = [];
+  for (const i of arr1) {
+    for (const j of arr2) {
+      for (const k of arr3) {
+        for (const l of arr4) {
+          res.push(`${i}${j}${k}${l}`);
+        }
+      }
+    }
+  }
+
+  console.log(
+    `Estas son las ${res.length} combinaciones posibles para tu dominio: `
+  );
+  for (const item of res) {
+    console.log(item);
+  }
+}
